b2105c600bef3fc816103e2d5e38d7f38e334342
Karloid <krldboyan@gmail.com>
2013-01-23 00:36:08 +0400

    Update MyStrategy.java

--- MyStrategy.java
+++ MyStrategy.java
@@ -1,5 +1,6 @@
 import model.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static java.lang.StrictMath.PI;
@@ -8,8 +9,10 @@ public final class MyStrategy implements Strategy {
     long meters;
     double minAgle = PI / 180;
     double angleToTarget;
-    String targetName; //= "SmartGuy";
+    String targetName;// = "SmartGuy";
+    static Long targetId;
     Unit moveTarget;
+    static volatile ArrayList<Long> moveTargets = new ArrayList<Long>();
     Tank self;
     World world;
     Move move;
@@ -17,6 +20,7 @@ public final class MyStrategy implements Strategy {
     private double lastPosY;
     private boolean stuck = false;
     private int stuckTick;
+    HashMap<Long, Tank> tanks;
 
     MyStrategy() {
 
@@ -27,24 +31,30 @@ public final class MyStrategy implements Strategy {
         this.self = self;
         this.world = world;
         this.move = move;
-        HashMap<String, Tank> tanks = new HashMap<String, Tank>();
+        tanks = new HashMap<Long, Tank>();
         for (Tank tank : world.getTanks()) {
-            tanks.put(tank.getPlayerName(), tank);
+            tanks.put(tank.getId(), tank);
         }
 
-        if (targetName != null && tanks.get(targetName).getCrewHealth() == 0) {
-            targetName = null;
+        if (targetId != null && (tanks.get(targetId).getCrewHealth() == 0 || tanks.get(targetId).getHullDurability() == 0)) {
+            targetId = null;
         }
         for (Tank tank : tanks.values())
-            if (!tank.isTeammate() && tank.getCrewHealth() != 0 && (targetName == null || tank.getPlayerName().equals(targetName))) {
-                angleToTarget = self.getTurretAngleTo(tank);
-                move.setTurretTurn(angleToTarget);
-                if (Math.abs(angleToTarget) > minAgle) {
-                    move.setFireType(FireType.NONE);
-                } else {
-                    move.setFireType(FireType.PREMIUM_PREFERRED);
+            if (!tank.isTeammate() && (tank.getCrewHealth() != 0 && tank.getHullDurability() != 0) && (targetId == null || ((Long) tank.getId()).equals(targetId))) {
+                if (tank.getPlayerName().equals(targetName) || targetIsDead()) {
+                    angleToTarget = self.getTurretAngleTo(tank);
+                    move.setTurretTurn(angleToTarget);
+                    if (Math.abs(angleToTarget) > minAgle) {
+                        move.setFireType(FireType.NONE);
+                    } else {
+                        targetId = tank.getId();
+                        if (noTeammateOnFireLine(tank)) {
+                            move.setFireType(FireType.PREMIUM_PREFERRED);
+                        } else {
+                            move.setFireType(FireType.NONE);
+                        }
+                    }
                 }
-
             }
         Bonus[] bonuses = world.getBonuses();
         double minDistanceToBonus = 2000;
@@ -61,16 +71,19 @@ public final class MyStrategy implements Strategy {
             if (distance < minDistanceToBonus) {
                 minDistanceToBonus = distance;
             }
-            if (angle < minAngleToBonus) {
+            if (!moveTargets.contains(bonus.getId())) {
+                // System.out.println("Bonus claim teammate: " + bonus.getId());
+            }
+            if (angle < minAngleToBonus && !moveTargets.contains(bonus.getId())) {
                 minAngleToBonus = angle;
             }
 
-            if (bonus.getType().equals(BonusType.MEDIKIT) && angle < minAngleToMedkit && self.getCrewHealth() != 100 ) {
+            if (bonus.getType().equals(BonusType.MEDIKIT) && angle < minAngleToMedkit && self.getCrewHealth() != 100 && !moveTargets.contains(bonus.getId())) {
                 minAngleToMedkit = angle;
             }
         }
-        if (bonusNoExists && moveTarget!=null) {
-            System.out.println(" REMOVE MOVE TARGET: " + moveTarget.getId());
+        if (bonusNoExists && moveTarget != null) {
+            moveTargets.remove(moveTarget.getId());
             moveTarget = null;
 
         }
@@ -84,10 +97,12 @@ public final class MyStrategy implements Strategy {
                 moved = true;
             } else if (bonus.getType().equals(BonusType.MEDIKIT) && angle == minAngleToMedkit) {
                 moveTarget = bonus;
+                moveTargets.add(moveTarget.getId());
                 moveTo(moveTarget);
                 moved = true;
             } else if (angle == minAngleToBonus) {
                 moveTarget = bonus;
+                moveTargets.add(moveTarget.getId());
                 moveTo(moveTarget);
                 moved = true;
             }
@@ -100,6 +115,26 @@ public final class MyStrategy implements Strategy {
 
     }
 
+    private boolean noTeammateOnFireLine(Unit unit) {
+        double distance = self.getDistanceTo(unit);
+        for (Tank tank : tanks.values()) {
+            if (tank.isTeammate() && self.getDistanceTo(tank) < distance && Math.abs(self.getTurretAngleTo(tank)) < minAgle * 8) {
+                System.out.println("Teammate on fire line!");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean targetIsDead() {
+        for (Tank tank : world.getTanks()) {
+            if (tank.getPlayerName().equals(targetName) && tank.getCrewHealth() != 0) {
+                return false;
+            }
+        }
+        return true;  //To change body of created methods use File | Settings | File Templates.
+    }
+
     private void moveBack() {
         move.setLeftTrackPower(-0.6D);
         move.setRightTrackPower(-0.6D);
@@ -107,7 +142,10 @@ public final class MyStrategy implements Strategy {
 
     private void moveTo(Unit unit) {
         double angleToUnit = self.getAngleTo(unit);
-       // System.out.println("move to" + unit.getId() + " type:" + ((Bonus) unit).getType() + " distance: " + self.getDistanceTo(unit) + " pos x  y" + unit.getX() + " " + unit.getY());
+        // System.out.println("move to" + unit.getId() + " type:" + ((Bonus) unit).getType() + " distance: " + self.getDistanceTo(unit) + " pos x  y" + unit.getX() + " " + unit.getY());
+        if (evade()) {
+            return;
+        }
         if (unstuck()) {
             return;
         }
@@ -126,8 +164,39 @@ public final class MyStrategy implements Strategy {
         }
     }
 
+    private boolean evade() {
+        for (Shell shell : world.getShells()) {
+            if (Math.abs(shell.getAngleTo(self)) < minAgle * 2) {
+                System.out.println("EVADE SHELL FAST FORWARD " + self.getId());
+                move.setLeftTrackPower(1D);
+                move.setRightTrackPower(1D);
+                unstuck();
+                return true;
+            }
+        }
+        for (Tank tank : world.getTanks()) {
+            if (Math.abs(tank.getTurretAngleTo(self)) < minAgle * 2 && tank.getRemainingReloadingTime() < 80 && isAlive(tank)) {
+                System.out.println("EVADE TURRENT FAST FORWARD " + self.getId() + " remaining reloading time()" + tank.getRemainingReloadingTime() + " max" + tank.getReloadingTime());
+                move.setLeftTrackPower(1D);
+                move.setRightTrackPower(1D);
+                unstuck();
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+    private boolean isAlive(Tank tank) {
+        if (tank.getCrewHealth() > 0 && tank.getHullDurability() > 0) {
+            return true;
+        }
+        return false;
+
+    }
+
     private boolean unstuck() {
-        if (stuck && world.getTick() - stuckTick < 30) {
+        if (stuck && world.getTick() - stuckTick < 100) {
             moveBack();
             lastPosX = 0;
             lastPosY = 0;
